12cef7f51cbe5e17555a0a97e0acfaba259f98df
rawtoast <jim@jamesmorris.org>
2015-10-20 18:13:20 +0100

    Added delete call, tidied index, added back button to new page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,12 +13,35 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
-      render 'new'
+      render new
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    if @article.save
+      redirect_to @article
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 
 end
