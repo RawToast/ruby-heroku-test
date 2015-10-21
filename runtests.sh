ba1406b469fcc95569948d52482176f1a1da663c
rawtoast <jim@jamesmorris.org>
2015-10-21 15:31:57 +0100

    Push to heroku?

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -ev
 # Run ruby unit TestCase
+echo ##################
+echo Running unit tests
+echo ##################
 bundle exec rake
+
+# Push to dummy application
+echo ##################
+echo Pushing to STAGING
+echo ##################
+git remote add heroku git@heroku.com:rocky-scrubland-staging.git
+git push heroku master
+
 # Run selenium tests
+echo ######################
+echo Running selenium tests
+echo ######################
 nosetests --nocapture selenium/dummy_test.py
