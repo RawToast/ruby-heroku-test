6ffc217664df8a4b86ba305b720734ec4ba50a58
rawtoast <jim@jamesmorris.org>
2015-10-21 15:40:14 +0100

    Quiet git push

--- runtests.sh
+++ runtests.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 set -ev
 # Run ruby unit TestCase
-echo ##################
-echo Running unit tests
-echo ##################
 bundle exec rake
 
 # Push to dummy application
-echo ##################
-echo Pushing to STAGING
-echo ##################
 git remote add heroku git@heroku.com:rocky-scrubland-staging.git
-git push heroku master
+git push heroku master -q
 
 # Run selenium tests
-echo ######################
-echo Running selenium tests
-echo ######################
 nosetests --nocapture selenium/dummy_test.py
